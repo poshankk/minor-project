700de88a2a15e7f581d414cd4c7dab888341b120
poshankk <36693629+poshankk@users.noreply.github.com>
2018-02-22 22:15:06 +0530

    Add files via upload

---
+++ flam_email.ino
@@ -0,0 +1,66 @@
+#define BLYNK_PRINT Serial
+#include<SPI.h>
+#include<ESP8266WiFi.h>
+#include<BlynkSimpleEsp8266.h>
+ BlynkTimer timer;
+
+ char auth[] = "5c611392491949dcbc5e3343b1956e47";
+
+ char ssid[] = "POSHAN";
+ char pass[] = "Golu@.43061";
+
+ int led = 13;
+ int pin = 2;
+ //int analog = A0;
+ int val;
+ //float sensor;
+ 
+
+ 
+ 
+ void flame()
+     {
+
+        // sensor = analogRead(analog);
+        
+         long uptime = millis() / 1000L;
+         
+          val = digitalRead(pin);
+         if(val==HIGH)
+         {
+
+           
+           digitalWrite(led,HIGH);
+           Blynk.notify("Fire detected");
+           Blynk.email("poshankk43@gmail.com", "Subject: FIRE DETECTION", "Fire Detected at NIT RAIPUR");
+          }
+          else
+          {
+            Serial.println("Fire not detected");
+            digitalWrite(led,LOW);
+          }
+          delay(500);
+     }
+
+     void setup() 
+     {
+      
+       pinMode(led,OUTPUT);
+       pinMode(pin,INPUT);
+      // pinMode (analog, INPUT) ;
+       Blynk.begin(auth, ssid, pass);
+       Blynk.notify("wifi connected");
+       Blynk.email("poshankk43@gmail.com", "Subject: WIFI STATUS", "Wifi is online...");
+       attachInterrupt(digitalPinToInterrupt(pin), flame, CHANGE);
+       timer.setInterval(1000L, flame);
+
+}
+     
+
+
+void loop() 
+{
+  Blynk.run();
+  timer.run();
+  
+}
